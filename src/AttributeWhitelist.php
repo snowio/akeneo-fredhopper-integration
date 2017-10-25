35c5084b1af5995cfeedb25819f402459187b620
Josh Di Fabio <joshdifabio@gmail.com>
2017-10-25 16:27:17 +0100

    Use strict types

--- src/AttributeWhitelist.php
+++ src/AttributeWhitelist.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 namespace SnowIO\AkeneoFredhopper;
 
 use SnowIO\AkeneoDataModel\AttributeData;
--- src/Mapper/LocalizableAttributeValueMapper.php
+++ src/Mapper/LocalizableAttributeValueMapper.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 namespace SnowIO\AkeneoFredhopper\Mapper;
 
 use SnowIO\AkeneoDataModel\AttributeValue as AkeneoAttributeValue;
